c9d5698e390968e075364d4c7d017278a0fb298b
Filip A <filip.aldenhov@gmail.com>
2021-03-26 12:57:16 +0100

    la till dropout layer

--- Backend/CNN/Loss_functions.py
+++ Backend/CNN/Loss_functions.py
@@ -77,3 +77,16 @@ class Act_Softmax_Loss_CCentropy():
         # Normalisera
         self.dinputs = self.dinputs / samples
         
+class Dropout:
+    def __init__(self, rate):
+        # Dropout rate (rate), procenttal på andel av neuroner som slås av vid varje forward pass
+        self.rate = 1 - rate
+
+    def forward(self, inputs):
+        self.inputs = inputs
+
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
