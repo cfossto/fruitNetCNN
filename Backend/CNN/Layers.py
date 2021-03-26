9fb883bc575d65d753219d96959bea909e31c43b
Filip A <filip.aldenhov@gmail.com>
2021-03-26 15:41:07 +0100

    merge conv + max pool

--- Backend/CNN/Layers.py
+++ Backend/CNN/Layers.py
@@ -48,3 +48,129 @@ class Dense_layer:
         self.dinputs = np.dot(dvalues, self.weights.T)
 
 
+class Conv_layer:
+    def __init__(self, name, filters=16, stride=1, size=3):
+        self.name = name
+        self.filters = np.random.randn(filters, 3, 3) * 0.1
+        self.stride = stride
+        self.size = size
+        self.last_input = None
+
+    def forward(self, inputs):
+        self.last_input = inputs
+        input_dimension = inputs.shape[1]
+        output_dimension = int((input_dimension - self.size) / self.stride) + 1
+        out = np.zeros((self.filters.shape[0], output_dimension, output_dimension))
+
+        for i in range(self.filters.shape[0]):
+            out_y = 0
+            tmp_y = out_y
+            while tmp_y + self.size <= input_dimension:
+                out_x = 0
+                tmp_x = out_x 
+                while tmp_x + self.size <= input_dimension:
+                    patch = inputs[:, tmp_y:tmp_y + self.size, tmp_x:tmp_x + self.size]
+                    out[i, out_y, out_x] += np.sum(self.filters[i] * patch)
+                    tmp_x += self.stride
+                    out_x += 1
+                tmp_y += self.stride
+                out_y += 1
+        self.output = out
+
+    def backward(self, dvalues, learn_rate=0.005):
+        input_dimension = self.last_input.shape[1]
+        doutput = np.zeros(self.last_input.shape)
+        dfilt = np.zeros(self.filters.shape)
+
+        for i in range(self.filters.shape[0]):
+            out_y = 0
+            tmp_y = out_y
+            while tmp_y + self.size <= input_dimension:
+                out_x = 0
+                tmp_x = out_x
+                while tmp_x + self.size <= input_dimension:
+                    patch = self.last_input[:, tmp_y:tmp_y + self.size, tmp_x:tmp_x + self.size]
+                    dfilt[i] += np.sum(dvalues[i, out_y, out_x] * patch, axis=0)
+                    doutput[:, tmp_y:tmp_y + self.size, tmp_x:tmp_x + self.size] += dvalues[i, out_y, out_x] * self.filters[i]
+
+                    tmp_x += self.stride
+                    out_x += 1
+                tmp_y += self.stride
+                out_y += 1
+        self.filters -= learn_rate * dfilt
+        self.output = doutput
+
+    def get_weights(self):
+        self.output = np.reshape(self.filters, -1)
+
+class Pooling_layer:
+    def __init__(self, name, stride=1, size=2):
+        self.last_input = None
+        self.stride = stride
+        self.size = size
+
+    def forward(self, inputs):
+        self.last_input = inputs
+
+        num_channels, h_prev, w_prev = inputs.shape
+        h = int((h_prev - self.size) / self.stride) + 1
+        w = int((w_prev - self.size) / self.stride) + 1
+
+        downsampled = np.zeros((num_channels, h, w))
+
+        for i in range(num_channels):
+            out_y = 0
+            curr_y = out_y
+            while curr_y + self.size <= h_prev:
+                out_x = 0
+                curr_x = out_x
+                while curr_x + self.size <= w_prev:
+                    patch = inputs[i, curr_y:curr_y + self.size, curr_x:curr_x + self.size]
+                    downsampled[i, out_y, out_x] = np.max(patch)
+                    curr_x += self.stride
+                    out_x += 1
+                curr_y += self.stride
+                out_y += 1
+        
+        self.output = downsampled
+
+    def backward(self, din, learning_rate):
+        num_channels, orig_dim, *_ = self.last_input.shape
+
+        doutput = np.zeros(self.last_input.shape)
+
+        for c in range(num_channels):
+            out_y = 0
+            tmp_y = out_y
+            while tmp_y + self.size <= orig_dim:
+                out_x = 0
+                tmp_x = out_x
+                while tmp_x + self.size <= orig_dim:
+                    patch = self.last_input[c, tmp_y:tmp_y + self.size, tmp_x:tmp_x + self.size]
+                    (x,y) = np.unravel_index(np.nanargmax(patch), patch.shape)
+                    doutput[c, tmp_y + x, tmp_x + y] += din[c, out_y, out_x]
+                    tmp_x += self.stride
+                    out_x += 1
+                tmp_y += self.stride
+                out_y += 1
+        
+        self.output = doutput
+
+    def get_weights(self):
+        self.output = 0
+            
+
+
+class Dropout_layer:
+    def __init__(self, rate):
+        # Dropout rate (rate), procenttal på andel av neuroner som slås av vid varje forward pass
+        self.rate = 1 - rate
+
+    def forward(self, inputs):
+        self.inputs = inputs
+
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
