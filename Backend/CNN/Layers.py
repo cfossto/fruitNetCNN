9352a06cfacd60e2435aaf9496ab7c31cb22d734
Filip A <filip.aldenhov@gmail.com>
2021-03-26 15:21:02 +0100

    flyttade dropout klassen till layers

--- Backend/CNN/Layers.py
+++ Backend/CNN/Layers.py
@@ -106,3 +106,16 @@ class Pooling_layer:
             
 
 
+class Dropout_layer:
+    def __init__(self, rate):
+        # Dropout rate (rate), procenttal på andel av neuroner som slås av vid varje forward pass
+        self.rate = 1 - rate
+
+    def forward(self, inputs):
+        self.inputs = inputs
+
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
