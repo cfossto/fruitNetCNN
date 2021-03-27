40d1bf4669acb0c18a84c895c634ebb0bf301f8b
cfossto <70951673+cfossto@users.noreply.github.com>
2021-03-27 15:47:17 +0100

    comments

--- API_gateway.py
+++ API_gateway.py
@@ -6,18 +6,22 @@ from Backend.processing import imageProcess as img
 app = Flask(__name__,static_folder="static")
 port = 9999
 
+
+
+# Route for root
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
 
 
+# Route for image dropper
+# Should get an image in the request.
+# Send that image to imageProcess
 @app.route("/imageSend",methods=["POST"])
 def image_send():
-    hello = img.greeting()
-    product = img.process(4)
-    return str(product) + hello
-        
-
+    hello = img.greeting() # testing import from imageProcess
+    product = img.process(4) # function with argument from imageProcess
+    return str(product) + " " + hello # this is how you return multiple results
 
 
 
